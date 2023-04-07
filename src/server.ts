86a5487dc2243fe652672aeba04446115a61f535
Abhinand <abhinandkofficial01@gmail.com>
2023-04-07 23:12:30 +0530

    mongoose connection and env

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
-import express from "express";
-const app = express();
-const port = 5000;
+import app from "./app";
+import env from "./util/validateEnv"
+import mongoose from "mongoose";
 
-app.get("/", (req, res) => {
-    res.send("Hello, Abhi!")
-});
+const port = env.PORT;
 
-app.listen(port, () => {
-    console.log("Server running on port" + port);
-    
-})
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+    .then(() => {
+        console.log("Mongoose Connected");
+        app.listen(port, () => {
+            console.log("Server running on port" + port);
+        })
+    }).catch((error) => {
+        console.log(error);
+        
+    })
+
